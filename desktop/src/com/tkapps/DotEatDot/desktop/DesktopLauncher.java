67100876cc04a1bc0edd10bbf235a3644610da31
tkimia <tkimia@purdue.edu>
2014-05-03 13:51:30 -0400

    Initial commit

---
+++ desktop/src/com/tkapps/DotEatDot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tkapps.DotEatDot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tkapps.DotEatDot.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
